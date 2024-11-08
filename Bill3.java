bc71c295631a25aabcd5a6950b8bafd6874dcfac
yarden raab <yarden.raab@gmail.com>
2024-11-08 08:48:22 +0200

    Bill Three

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,9 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		int price = Integer.parseInt(args[3]);
+	    System.out.println("Dear "+ name1 + ", " + name2 + ", " + ", and " + name3 + ": pay " + Math.ceil(price/3.0) + " shekel each.");
 	}
 }
